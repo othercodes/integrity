741b2ae1ecc9bf5a820a7da7bb49aa519c25999d
David Unay Santisteban <slavepens@gmail.com>
2014-03-16 15:26:39 +0100

    Create checkMD5.php

---
+++ checkMD5.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        include 'libraries/integrity.md5.class.php';
+        $integrity = new integrity('./');
+        $files = $integrity->checkMD5Hashes('20140316151603.md5');
+        ?>
+        <h2>Modified Files</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>File</th>
+                    <th>MD5Hass</th>
+                    <th>User ID</th>
+                    <th>Group ID</th>
+                    <th>Last Access</th>
+                    <th>Last Modified</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php foreach($files as $file) { ?>
+                <tr>
+                    <td><?php echo $file['filename']; ?></td>
+                    <td><?php echo $file['md5Hash']; ?></td>
+                    <td><?php echo $file['uid']; ?></td>
+                    <td><?php echo $file['gid']; ?></td>
+                    <td><?php echo $file['lastAccess']; ?></td>
+                    <td><?php echo $file['lastModification']; ?></td>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+    </body>
+</html>
+
