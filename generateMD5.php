3414a0b8ec652a546bfec64360b385dcc8b5887b
David Unay Santisteban <slavepens@gmail.com>
2014-04-02 01:23:23 +0200

    Update generateMD5.php

--- generateMD5.php
+++ generateMD5.php
@@ -1,7 +1,7 @@
 <?php
 include 'libraries/integrity.md5.class.php';
 
-$integrity = new integrity('./');
+$integrity = new integrity('path/to/check/directory');
 
 if($integrity->getMd5Hashes('hash.md5') > 0){
     echo "MD5 File Hashes Generated!";
